753364e6dc15427a6258ae9858ea4dbf0e48792c
dfoldesi <dalma@localprojects.net>
2017-04-03 18:39:55 -0400

    Update state manager with state IDs from grid

--- BRT_UNITY/Assets/Resources/scripts/GridStateManager.cs
+++ BRT_UNITY/Assets/Resources/scripts/GridStateManager.cs
@@ -14,7 +14,17 @@ public class GridStateManager : MonoBehaviour
 	private GameObject gameObjClone;
 	private int stateToDeploy;
 
-	List<int> MarkerList = new List<int> ();
+	//List<int> MarkerList = new List<int> ();
+	//public static int[,] Scanners.currentIds;
+
+
+	private string[] stateNames = {
+		"Error",
+		"CurrentStreet",
+		"GoldBRT",
+		"SilverBRT",
+		"GoldBRT & Bike"
+	};
 
 	// Use this for initialization
 	void Start ()
@@ -23,57 +33,18 @@ public class GridStateManager : MonoBehaviour
 		checkMarkerList ();
 	}
 
-	void OnMarkerFound (int marker)
-	{
-
-		MarkerList.Add (marker); //add found markers to list
-
-		Debug.Log ("FOUND >> " + marker + " |  The list has " + MarkerList.Count + " markers now.");
-		for (int i = 0; i < MarkerList.Count; i++) {
-			Debug.Log ("Item: " + i + " in list tag is: " + MarkerList [i]);
-
-		}
-
-		checkMarkerList ();
-
-	}
-
-
-	void OnMarkerLost (int marker)
-	{
-		MarkerList.Remove (marker);
-
-		Debug.Log ("LOST << " + marker + " |  The list has " + MarkerList.Count + " markers now.");
-		for (int i = 0; i < MarkerList.Count; i++) {
-			Debug.Log ("Item: " + i + " in list tag is: " + MarkerList [i]);
-
-		}
-		checkMarkerList ();
-	}
-
 	private void checkMarkerList ()
 	{
-		
-		if (MarkerList.Count == 0) {
+		if (Scanners.currentIds.Length == 0) {
 			StateMan (0);
-		} 
-//		else if (MarkerList.Count == 1) {
-//			if (MarkerList [0].Tag.Equals ("GoldBRT")) {
-//				StateMan (3);
-//
-//			} else if (MarkerList [0].Tag.Equals ("BikeLane")) {
-//				StateMan (0);
-//			} 
-//		} else if (MarkerList.Count == 2) {
-//			if (MarkerList [0].Tag.Equals ("GoldBRT") && MarkerList [1].Tag.Equals ("BikeLane") || MarkerList [1].Tag.Equals ("GoldBRT") && MarkerList [0].Tag.Equals ("BikeLane")) {
-//				StateMan (4);
-//			} else {
-//				StateMan (0);
-//			}
-//
-//			MarkerList.Clear ();
-//			Debug.Log ("cleared list!");	
-//		}
+		} else if (Scanners.currentIds [0, 0] == 2 && Scanners.currentIds [0, 1] == -1) {
+			StateMan (3);
+		} else if ((Scanners.currentIds [0, 1] == 4 && Scanners.currentIds [0, 0] == 2) || (Scanners.currentIds [0, 1] == 2 && Scanners.currentIds [0, 0] == 4)) {
+			StateMan (4);
+		} else {
+			StateMan (0);
+		}
+		Debug.Log ("State change.");	
 	}
 
 
